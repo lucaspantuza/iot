d06136d7f29a13987e76b31faa472fd27229b922
lucaspantuza <lucaspa@gmail.com>
2023-05-20 02:18:50 -0300

    inclusao de arquivos do cv e tensorflow lucas

---
+++ PROJETO visao computacional/exemplos/cv_teste.py
@@ -0,0 +1,8 @@
+# teste do opencv
+import cv2
+img = cv2.imread('G:\Meu Drive\DEV\iot\PROJETO visao computacional\exemplos\OpenCV_Logo.png', cv2.IMREAD_COLOR)
+cv2.namedWindow('Hello World')
+cv2.imshow('Hello World', img)
+cv2.waitKey()
+
+
