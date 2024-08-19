cbacd03e027ad332de7adfbf555044660df9cad3
LMeckler <148484446+LMeckler@users.noreply.github.com>
2024-08-19 15:03:05 -0300

    Update Baratinha.ino

--- PROJETO presskill - LucasMackler/code/Baratinha.ino
+++ PROJETO presskill - LucasMackler/code/Baratinha.ino
@@ -1,31 +1,5 @@
-/*  Para Larissa 
-  
+//Para Larissa 
 
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-
-// selecionar a placa "ESP32 Dev Module"
 
 int x = 0;
 int y = 0;
@@ -79,7 +53,7 @@ byte yalp[] = {
   B01110,
   B00110,
   B00010
-};
+};//
 
 byte caveira[] = {
   B00000,
@@ -145,7 +119,7 @@ byte snow[] = {
   B01110,
   B00100,
   B10000
-};
+};//
 
 
 //  Variáveis menu
@@ -356,18 +330,7 @@ void takeOnMeSemLuzinha () { //toca Take On Me (não pisca os leds)
     NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
     REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
     NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
-    REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
-    NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
-        
-    REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
-    NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
-    REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
-    NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
-    REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
-        
-    NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
-    REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
-        
+    REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,    
   };
 
   // sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
@@ -480,6 +443,7 @@ void furElise() { //toca furElise //nota1
 
       // Wait for the specief duration before playing the next note.
       delay(noteDuration);
+      encerraJogo();
 
       // stop the waveform generation before the next note.
       noTone(buzzer);
@@ -639,6 +603,153 @@ void takeOnMe () { //toca Take On Me (pisca os leds)  //maquina
 
     luzinha(); //faz os LEDs piscarem aleatoriamente durante a música
     encerraJogo();
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+
+    // stop the waveform generation before the next note.
+    noTone(BUZZER);
+  }
+}
+
+void takeOnMeDemo () { //toca Take On Me (pisca os leds)  //maquina
+  #define NOTE_B0  31
+  #define NOTE_C1  33
+  #define NOTE_CS1 35
+  #define NOTE_D1  37
+  #define NOTE_DS1 39
+  #define NOTE_E1  41
+  #define NOTE_F1  44
+  #define NOTE_FS1 46
+  #define NOTE_G1  49
+  #define NOTE_GS1 52
+  #define NOTE_A1  55
+  #define NOTE_AS1 58
+  #define NOTE_B1  62
+  #define NOTE_C2  65
+  #define NOTE_CS2 69
+  #define NOTE_D2  73
+  #define NOTE_DS2 78
+  #define NOTE_E2  82
+  #define NOTE_F2  87
+  #define NOTE_FS2 93
+  #define NOTE_G2  98
+  #define NOTE_GS2 104
+  #define NOTE_A2  110
+  #define NOTE_AS2 117
+  #define NOTE_B2  123
+  #define NOTE_C3  131
+  #define NOTE_CS3 139
+  #define NOTE_D3  147
+  #define NOTE_DS3 156
+  #define NOTE_E3  165
+  #define NOTE_F3  175
+  #define NOTE_FS3 185
+  #define NOTE_G3  196
+  #define NOTE_GS3 208
+  #define NOTE_A3  220
+  #define NOTE_AS3 233
+  #define NOTE_B3  247
+  #define NOTE_C4  262
+  #define NOTE_CS4 277
+  #define NOTE_D4  294
+  #define NOTE_DS4 311
+  #define NOTE_E4  330
+  #define NOTE_F4  349
+  #define NOTE_FS4 370
+  #define NOTE_G4  392
+  #define NOTE_GS4 415
+  #define NOTE_A4  440
+  #define NOTE_AS4 466
+  #define NOTE_B4  494
+  #define NOTE_C5  523
+  #define NOTE_CS5 554
+  #define NOTE_D5  587
+  #define NOTE_DS5 622
+  #define NOTE_E5  659
+  #define NOTE_F5  698
+  #define NOTE_FS5 740
+  #define NOTE_G5  784
+  #define NOTE_GS5 831
+  #define NOTE_A5  880
+  #define NOTE_AS5 932
+  #define NOTE_B5  988
+  #define NOTE_C6  1047
+  #define NOTE_CS6 1109
+  #define NOTE_D6  1175
+  #define NOTE_DS6 1245
+  #define NOTE_E6  1319
+  #define NOTE_F6  1397
+  #define NOTE_FS6 1480
+  #define NOTE_G6  1568
+  #define NOTE_GS6 1661
+  #define NOTE_A6  1760
+  #define NOTE_AS6 1865
+  #define NOTE_B6  1976
+  #define NOTE_C7  2093
+  #define NOTE_CS7 2217
+  #define NOTE_D7  2349
+  #define NOTE_DS7 2489
+  #define NOTE_E7  2637
+  #define NOTE_F7  2794
+  #define NOTE_FS7 2960
+  #define NOTE_G7  3136
+  #define NOTE_GS7 3322
+  #define NOTE_A7  3520
+  #define NOTE_AS7 3729
+  #define NOTE_B7  3951
+  #define NOTE_C8  4186
+  #define NOTE_CS8 4435
+  #define NOTE_D8  4699
+  #define NOTE_DS8 4978
+  #define REST      0
+
+  int tempo = 140;
+
+  // notes of the moledy followed by the duration.
+  // a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+  // !!negative numbers are used to represent dotted notes,
+  // so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+  int melody[] = {
+
+    // Take on me, by A-ha
+    // Score available at https://musescore.com/user/27103612/scores/4834399
+    // Arranged by Edward Truong
+
+    NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
+    REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
+    NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
+    REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,       
+  };
+
+  // sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+  // there are two values per note (pitch and duration), so for each note there are four bytes
+  int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+  // this calculates the duration of a whole note in ms
+  int wholenote = (60000 * 4) / tempo;
+
+  int divider = 0, noteDuration = 0;
+
+  // iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(BUZZER, melody[thisNote], noteDuration * 0.9);
+
+    luzinha(); //faz os LEDs piscarem aleatoriamente durante a música
 
     // Wait for the specief duration before playing the next note.
     delay(noteDuration);
@@ -819,6 +930,7 @@ void theGodfather () {  //toca The Godfather Theme (O Poderoso Chefão)  //cavei
 
     // Wait for the specief duration before playing the next note.
     delay(noteDuration);
+    encerraJogo();
 
     // stop the waveform generation before the next note.
     noTone(BUZZER);
@@ -996,6 +1108,7 @@ void silentNight () { //toca Silent Night //snow
 
     // Wait for the specief duration before playing the next note.
     delay(noteDuration);
+    encerraJogo();
 
     // stop the waveform generation before the next note.
     noTone(BUZZER);
@@ -1159,6 +1272,7 @@ void brahms () { //toca Brahms’ Lullaby (Wiegenlied)  //nota2
 
     // Wait for the specief duration before playing the next note.
     delay(noteDuration);
+    encerraJogo();
 
     // stop the waveform generation before the next note.
     noTone(BUZZER);
@@ -1331,6 +1445,7 @@ void tetris () { //toca tema de tetris e verifica modo de jogo  //tetris
 
     // Wait for the specief duration before playing the next note.
     delay(noteDuration);
+    encerraJogo();
     
     // stop the waveform generation before the next note.
     noTone(BUZZER);
@@ -1830,7 +1945,7 @@ void perdeJogo () {
     lcd.setCursor(0, 1);
     lcd.print("Novo recorde->");
     lcd.print(recorde);
-    takeOnMe();
+    takeOnMeDemo();
     // EEPROM.write(enderecoRecorde, recorde);
     // EEPROM.commit();  // Isso é importante para gravar os dados na EEPROM
   }
@@ -1871,7 +1986,7 @@ void ganhouJogo () {
   lcd.setCursor(2, 1);
   lcd.print("VOCE VENCEU!");
 
-  takeOnMe();
+  takeOnMeDemo();
   Serial.print("ganhouJogo()");
 
   for (int i = 0; i < 20; i++) {
