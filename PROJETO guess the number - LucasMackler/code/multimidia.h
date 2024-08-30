73c1d945a93655365639f482435e19c56f1cdbcb
Lucas Pantuza <lucaspa@gmail.com>
2024-08-30 14:08:29 -0300

    lucas sexta

---
+++ PROJETO guess the number - LucasMackler/code/multimidia.h
@@ -0,0 +1,66 @@
+
+byte setaCima[] = {   //cria um caractere com uma seta para cima
+  B00100,
+  B01110,
+  B11111,
+  B00100,
+  B00100,
+  B00100,
+  B00100,
+  B00100
+};
+
+byte setaBaixo[] = {  //cria um caractere com uma seta para baixo
+  B00100,
+  B00100,
+  B00100,
+  B00100,
+  B00100,
+  B11111,
+  B01110,
+  B00100
+};
+
+byte coracao[] = {
+  B00000,
+  B01010,
+  B11111,
+  B11111,
+  B11111,
+  B01110,
+  B00100,
+  B00000
+};
+
+byte caveira[] = {
+  B00000,
+  B01110,
+  B10101,
+  B11011,
+  B01110,
+  B01110,
+  B00000,
+  B00000
+};
+
+byte notaMusical1[] = {
+  B00001,
+  B00011,
+  B00101,
+  B01001,
+  B01001,
+  B01011,
+  B11011,
+  B11000
+};
+
+byte notaMusical2[] = {
+  B00000,
+  B00000,
+  B00000,
+  B00100,
+  B00110,
+  B00100,
+  B01100,
+  B01100
+};
